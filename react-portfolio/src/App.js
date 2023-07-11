e4f6d64465ec7e7779e3b2af8149169e65fe8b4e
ashleytrollinger <ashley@trollingers.com>
2023-07-11 19:21:14 -0400

    I think all components are kind of done

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,13 +4,22 @@ import React from "react";
 //Importing all the React components
 import Header from "./components/Header";
 //Nav is part of the Header Component
-import Project from "./components/Project";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <main>
+        <About></About>
+        <Portfolio></Portfolio>
+        <Contact></Contact>
+        <Resume></Resume>
+      </main>
+      <footer></footer>
     </div>
   )
 }
---
+++ react-portfolio/src/components/Resume/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Porfolio() {
+    return (
+        <section>
+            <h2>Portfolio Section</h2>
+        </section>
+    )
+}
