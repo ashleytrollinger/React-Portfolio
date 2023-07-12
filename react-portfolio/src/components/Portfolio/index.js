ea3dcd5d7b33ec0ade1d86ec2800cabbae0bd732
ashleytrollinger <ashley@trollingers.com>
2023-07-12 15:32:20 -0400

    favicon.ico added

--- react-portfolio/src/components/Portfolio/index.js
+++ react-portfolio/src/components/Portfolio/index.js
@@ -22,32 +22,32 @@ function Portfolio() {
             image: "Find-My-Musician.png",
             live: "https://find-my-musician.herokuapp.com/",
             github: "https://github.com/ashleytrollinger/FindMyMusician"
-        }
-    ]
-    /* For the last 3 projects
-    {
-        name: "Weather Dashboard",
-        description: "Works with OpenWeatherMap to create a 7 day weather forcaster",
-        image:"Weather-Dashboard.png",
-        live:null,
-        github:"https://github.com/ashleytrollinger/Weather-Dashboard"
         },
-        {
-        name: "Password Generator",
-        description: "",
-        image:"",
-        live:"",
-        github:""
-        },
-        {
-        name: "",
-        description: "",
-        image:"",
-        live:"",
-        github:""
-        },
-        */
 
+
+        {
+            name: "Weather Dashboard",
+            description: "Works with OpenWeatherMap to create a 7 day weather forcaster",
+            image: "Weather-Dashboard.png",
+            live: null,
+            github: "https://github.com/ashleytrollinger/Weather-Dashboard"
+        },
+        {
+            name: "Password Generator",
+            description: "Enter in criteria for a passowrd and the computer will generate a random one that meets all the requirements!",
+            image: "Password-Generator.png",
+            live: null,
+            github: "https://github.com/ashleytrollinger/JavaScript-Password-Generator"
+        },
+        {
+            name: "Coming End of July",
+            description: "Social media platform that combines the music focus of Spotify with the social aspects of Instagram",
+            image: "Work-In-Progress.png",
+            live: null,
+            github: null
+        },
+
+    ]
 
 
     return (
@@ -66,16 +66,16 @@ function Portfolio() {
                     <li className="padding">
                         <Project projects={projects[2]}></Project>
                     </li>
-                    {/* For the Last 3 Projects
+
                     <li className="padding">
-                        <Project projects= {projects[0]}></Project>
+                        <Project projects={projects[3]}></Project>
                     </li>
                     <li className="padding">
-                        <Project projects= {projects[0]}></Project>
+                        <Project projects={projects[4]}></Project>
                     </li>
                     <li className="padding">
-                        <Project projects= {projects[0]}></Project>
-                    </li> */}
+                        <Project projects={projects[5]}></Project>
+                    </li>
                 </ul>
             </div>
         </section>
