f52b7224a95f86a8db87ed9a7d4b695396ef0b12
ashleytrollinger <ashley@trollingers.com>
2023-07-12 14:38:16 -0400

    styling

--- react-portfolio/src/components/Footer/index.js
+++ react-portfolio/src/components/Footer/index.js
@@ -2,8 +2,34 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer>
-            Footer
+
+        <footer className="footer">
+            <div>
+                <a
+                    href="https://github.com/ashleytrollinger"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        src={require("../../assets/logos/github-mark.png")}
+                        alt="Github"
+                        className="logo"
+                    ></img>
+                </a>
+            </div>
+            <div>
+                <a
+                    href="https://linkedin.com/in/ashley-trollinger-a3276826a/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        src={require("../../assets/logos/linkedin.png")}
+                        alt="LinkedIn"
+                        className="logo"
+                    ></img>
+                </a>
+            </div>
         </footer>
     )
 }
