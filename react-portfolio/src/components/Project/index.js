4761920af0da28cefc5a796697970c219444590f
ashleytrollinger <ashley@trollingers.com>
2023-07-11 22:34:30 -0400

    Projects index.js done

--- react-portfolio/src/components/Project/index.js
+++ react-portfolio/src/components/Project/index.js
@@ -1,10 +1,27 @@
-import React from "react";
-
-function Project() {
-    
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card"
+function Project(props) {
+    const currentProject = useState(props)[0].projects;
+    const name = currentProject.name;
+    const description = currentProject.description;
+    const image = currentProject.image;
+    const live = currentProject.live;
+    const github = currentProject.github;
     return (
-        <section>
-            <h2>Projects</h2>
-        </section>
-    )
+        <Card style={{ width: "18rem" }}>
+            <Card.Img
+                variant="top"
+                src={require('../../assets/images/${image}')}
+                className="card-image"></Card.Img>
+            <Card.Body>
+                <Card.Title className="card-title">{name}</Card.Title>
+                <Card.Text className="card-text">{description}</Card.Text>
+                <Card.Link href={live} target="_blank">App Link</Card.Link>
+                <br></br>
+                <Card.Link href={github} target="_blank">Github Repo</Card.Link>
+            </Card.Body>
+        </Card>
+    );
 }
+
+export default Project;
